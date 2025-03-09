23e58975ceeba08f0e5e0d7298f4ab820343e2fd
Raditya Dwisatrio Santoso <dwisatrioraditya@gmail.com>
2025-03-09 09:27:27 +0700

    feat: adding firebase store and configuration

--- backend-repo/src/index.ts
+++ backend-repo/src/index.ts
@@ -1,10 +1,16 @@
 import express, { Express, Request, Response } from "express";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./config/firebaseConfig";
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+app.get("/", async (req: Request, res: Response) => {
+  const querySnapshot = await getDocs(collection(db, "users"));
+  querySnapshot.forEach((doc) => {
+    console.log(doc.id, doc.data());
+  });
+  res.send("Hello Firebase!");
 });
 
 app.listen(port, () => {
